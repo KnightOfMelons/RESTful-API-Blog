ef7c955a283f379c36af743f7f99f31a60d8a8e3
KnightOfMelons <ilaprosmotrov@gmail.com>
2024-10-14 19:08:48 +0300

    Fixed problem with cyrillic text and problem with invalid values too

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify, abort
 from flask_sqlalchemy import SQLAlchemy
+import json
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:123456789qwe123!@localhost:5432/bd_for_junior_blog'
@@ -20,8 +21,12 @@ with app.app_context():
 @app.route('/posts', methods=['POST'])
 def create_post():
     data = request.get_json()
+
     if not data or 'title' not in data or 'content' not in data:
         return abort(400, description="Invalid input")
+
+    if not data['title'].strip() or not data['content'].strip():
+        return abort(400, description="Title and content cannot be empty")
 
     new_post = Post(title=data['title'], content=data['content'])
     db.session.add(new_post)
@@ -35,10 +40,16 @@ def create_post():
 @app.route('/posts', methods=['GET'])
 def get_posts():
     posts = Post.query.all()
-    return jsonify([{'id': post.id,
-                     'title': post.title,
-                     'content': post.content,
-                     'likes': post.likes} for post in posts])
+    posts_list = [{'id': post.id,
+                   'title': post.title,
+                   'content': post.content,
+                   'likes': post.likes} for post in posts]
+
+    return app.response_class(
+        response=json.dumps(posts_list, ensure_ascii=False), # ensure_ascii нужна для того, чтобы кириллица работала.
+        status=200,
+        mimetype='application/json'
+    )
 
 
 if __name__ == "__main__":
